1884865e9db0f81f634b3d79ef5b2a8614ace3e6
alinatoc <alinatoc@openit.com>
2015-09-03 11:43:46 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+require_once './system/IndexEngine.php';
+
+define( 'ROOT_PATH',
+
+        rtrim( str_replace( '\\', '/', realpath(__DIR__) ),'/' ).'/'
+
+);
+
+
+$index = new IndexEngine();
+$index->run();
---
+++ system/IndexEngine.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * Description of IndexEngine
+ */
+class IndexEngine {
+
+    const DEFAULT_PAGE = 'home';
+    const PAGE_PARAM_INDEX = 0;
+    const ACTION_PARAM_INDEX = 1;
+
+
+    public function __construct()
+    {
+        /**
+         * Startup processes
+         */
+
+        // all initialization takes place
+        error_reporting(E_ALL | E_STRICT);
+        // setting the internal character encoding
+        mb_internal_encoding('UTF-8');
+        // autoload
+        spl_autoload_register(array($this, 'autoload'));
+    }
+
+    public function autoload($classname)
+    {
+        $path = 'system/libraries/'.$classname.'.php';
+        $path = str_replace('\\', '/', $path);
+
+        require_once $path;
+    }
+
+    public function getPage($page=self::DEFAULT_PAGE)
+    {
+        for ( $index=0,reset($_GET); $index<count($_GET); next($_GET),$index++ )
+        {
+            $key = key($_GET);
+            if ( $index === self::PAGE_PARAM_INDEX )
+            {
+                $page = $key;
+                BREAK;
+            }
+        }
+        RETURN $page;
+    }
+
+
+
+    public function getBackboneFile($page=self::DEFAULT_PAGE)
+    {
+        $pagesJson = utils\Config::readJson(ROOT_PATH . 'config/pages.json');
+        $backbonePage = isset($pagesJson[$page]) ? $pagesJson[$page] : array($page);
+        if ( !isset($backbonePage['file']) )
+        {
+            $backbonePage['file'] = '404';
+        }
+        RETURN ROOT_PATH.'ui/Backbones/'.$backbonePage['file'].'.php';
+    }
+
+
+
+    public function getTemplateFile($page=self::DEFAULT_PAGE)
+    {
+        $pagesJson = utils\Config::readJson(ROOT_PATH . 'config/pages.json');
+        $defaultTemplate = utils\Config::get('main_template', ROOT_PATH.'config/application.ini');
+        $template = $defaultTemplate;
+        $page = !isset($pagesJson[$page]) ? '404' : $page;
+        if ( isset($pagesJson[$page]['template']) )
+        {
+            $template = $pagesJson[$page]['template'];
+        }
+
+        RETURN ROOT_PATH . 'ui/Templates/' . $template . '.template.phtml';
+    }
+
+
+
+    public function getViewFile($page=self::DEFAULT_PAGE)
+    {
+        $pagesJson = utils\Config::readJson(ROOT_PATH . 'config/pages.json');
+        $backbonePage = isset($pagesJson[$page]) ? $pagesJson[$page] : array($page);
+        if ( !isset($backbonePage['file']) )
+        {
+            $backbonePage['file'] = '404';
+        }
+        RETURN ROOT_PATH.'ui/Views/'.$backbonePage['file'].'.phtml';
+    }
+
+
+
+    public function run()
+    {
+        $page = $this->getPage();
+        REQUIRE_ONCE $this->getBackboneFile($page);
+
+        ob_start();
+        require_once $this->getViewFile($page);
+        $_VIEW = ob_get_clean();
+        REQUIRE_ONCE $this->getTemplateFile($page);
+    }
+
+}
