ffdb2aef8a8041ef608f2c7362bc26b282edf91d
unknown <rdognidon@MNL125WIN.svg.openit.local>
2015-09-07 13:08:45 +0800

    upload

--- assets/3rd/js/function.js
+++ assets/3rd/js/function.js
@@ -1,30 +1,26 @@
 $(document).ready(function(){
-	$(document).ready(function () {
-		var username;
-        $("#startForm").submit(function (e) {
-			username = $("#username").val();
-			e.preventDefault();
-            $("#spinner").show();
-			$.ajax({
-				type: "POST",
-				url: "?home2",
-				contentType: 'json',
-				success: function (response) {
-					if(response.success){
-							$("#change").html('');
-						console.log(response.data);
-						for (var i=0; i < response.data.length; i++)
-						{
-							var row = response.data[i];
-							if(row.employee == username)
-							{
-								$('#change').append(row.employee + ', ' + row.age + ', ' + row.company + '<br>');
-								break;
-							}
-						}
+	var username;
+	$("#startForm").submit(function (e) {
+		username = $("#username").val();
+		e.preventDefault();
+		$("#spinner").show();
+		$.ajax({
+			type: "POST",
+			url: "?home2",
+			contentType: 'json',
+			success: function (response) {
+				if(response.success){
+						$("#change").html('');
+					console.log(response.data);
+					for (var i=0; i < response.data.length; i++)
+			1		{
+						var row = response.data[i];
+						$('#change').append(row.employee + ', ' + row.age + ', ' + row.company + '<br>');
+						break;
 					}
-				},
-			});
-        });
-    });
+				}
+			},
+		});
+	});
 });
+
