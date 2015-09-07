446607550df89404930ec4e305dbb933a912398c
alinatoc <alinatoc@openit.com>
2015-09-07 14:44:07 +0800

    Fixed preg_match procedure on request URI

--- ui/Backbones/rest/check_username.php
+++ ui/Backbones/rest/check_username.php
@@ -15,18 +15,23 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ * [REST_META]
+ *
+ * username (string)
+ *
  */
 
 header('Content-type: application/json');
 
 require_once ROOT_PATH . 'includes/load_ad_usernames.php';
+$username = trim($_REQUEST['username']);
 
 if (!isset($_REQUEST['username']))
 {
     die(ModelResponse::InvalidRequest());
 }
-
-$username = trim($_REQUEST['username']);
 
 $matches = array();
 preg_match('/[A-Za-z0-9_\.]+/', $username, $matches);
@@ -35,6 +40,6 @@ if (sizeof($matches) > 0)
     $username = strtolower($matches[0]);
 
 
-$exists = (in_array($username, $users));
+$exists = has_account($username);
 
 echo new ModelResponse($exists, $exists ? 'Username exists' : 'User is not registered');
