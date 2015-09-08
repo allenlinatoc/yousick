b5dd0f288214e00a469025500ac8cf25e0c1b184
lromero <lromero@openit.com>
2015-09-08 13:39:31 +0800

    Finished all six reports.

--- assets/3rd/js/reports.js
+++ assets/3rd/js/reports.js
@@ -188,6 +188,7 @@ $('#report3-nav-item').click(function() {
                     categories: []
                 },
                 yAxis: {
+                    min: 0,
                     title: {
                         text: 'Total Filed Sick Leave'
                     }
@@ -204,13 +205,238 @@ $('#report3-nav-item').click(function() {
             };
 
             var length = data.data.collection.length;
-            var sl_count = [];
-
             $.each(data.data.collection, function (index, ind_data) {
                 var xIndex = $.inArray(ind_data.yearmonth, options.xAxis.categories);
                 if (xIndex == -1)
                 {
                     options.xAxis.categories.push(ind_data.yearmonth);
+                    xIndex = options.xAxis.categories.length-1;
+                }
+
+                var username = ind_data.username;
+                var seriesIndex = getIndex(options.series, username);
+                if (seriesIndex == -1)
+                {
+                    var seriesData = [];
+                    for(var i = 0; i < xIndex; i++)
+                    {
+                        seriesData.push(0);
+                    }
+                    options.series.push({ name: username, data: seriesData});
+
+                    seriesIndex = options.series.length-1;
+                }
+                else {
+                    if (options.series[seriesIndex].data.length < xIndex)
+                    for (var i = 0; i < xIndex; i++)
+                    {
+                        if (options.series[seriesIndex].data[i] == null)
+                            options.series[seriesIndex].data.push(0);
+                    }
+                }
+                options.series[seriesIndex].data.push(parseInt(ind_data.count));
+
+                if (length == index+1)
+                    $('#chart-container').highcharts(options);
+            });
+        }
+    });
+});
+
+$('#report4-nav-item').click(function() {
+    var $data = getData(window.BASE_URL + '/rest/reports/yearly');
+
+    $data.success(function (data) {
+        if (data.success)
+        {
+            var options = {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: $('#report4-nav-item').text()
+                },
+                xAxis: {
+                    categories: []
+                },
+                yAxis: {
+                    title: {
+                        text: 'Total Sick Leaves Filed'
+                    }
+                },
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        },
+                        enableMouseTracking: false
+                    }
+                },
+                series: []
+            };
+
+            var length = data.data.collection.length;
+            var sl_count = [];
+
+            $.each(data.data.collection, function (index, yearly_data) {
+                options.xAxis.categories.push(yearly_data.year);
+                sl_count.push(parseInt(yearly_data.count));
+
+                if (length == index+1)
+                {
+                    options.series.push({
+                        name: 'All',
+                        data: sl_count
+                    })
+                    $('#chart-container').highcharts(options);
+                }
+            });
+        }
+    });
+});
+
+$('#report5-nav-item').click(function() {
+    var $data = getData(window.BASE_URL + '/rest/reports/yearly?category=department');
+
+    $data.success(function (data) {
+        if (data.success)
+        {
+            var options = {
+                chart: {
+                    type: 'column'
+                },
+                title: {
+                    text: $('#report5-nav-item').text()
+                },
+                xAxis: {
+                    categories: []
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Total Sick Leaves Filed'
+                    },
+                    stackLabels: {
+                        enabled: true,
+                        style: {
+                            fontWeight: 'bold',
+                            color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                        }
+                    }
+                },
+                legend: {
+                    align: 'right',
+                    x: -30,
+                    verticalAlign: 'top',
+                    y: 25,
+                    floating: true,
+                    backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || 'white',
+                    borderColor: '#CCC',
+                    borderWidth: 1,
+                    shadow: false
+                },
+                tooltip: {
+                    formatter: function () {
+                        return '<b>' + this.x + '</b><br/>' +
+                            this.series.name + ': ' + this.y + '<br/>' +
+                            'Total: ' + this.point.stackTotal;
+                    }
+                },
+                plotOptions: {
+                    column: {
+                        stacking: 'normal',
+                        dataLabels: {
+                            enabled: true,
+                            color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+                            style: {
+                                textShadow: '0 0 3px black'
+                            }
+                        }
+                    }
+                },
+                series: []
+            };
+
+            var length = data.data.collection.length;
+            var sl_count = [];
+
+            $.each(data.data.collection, function (index, dept_data) {
+                var xIndex = $.inArray(dept_data.year, options.xAxis.categories);
+                if (xIndex == -1)
+                {
+                    options.xAxis.categories.push(dept_data.year);
+                    xIndex = options.xAxis.categories.length-1;
+                }
+
+                var dept = dept_data.department;
+                var seriesIndex = getIndex(options.series, dept);
+                if (seriesIndex == -1)
+                {
+                    var seriesData = [];
+                    for(var i = 0; i < xIndex; i++)
+                    {
+                        seriesData.push(0);
+                    }
+                    options.series.push({ name: dept, data: seriesData});
+
+                    seriesIndex = options.series.length-1;
+                }
+                else {
+                    if (options.series[seriesIndex].data.length < xIndex)
+                    for (var i = 0; i < xIndex; i++)
+                    {
+                        if (options.series[seriesIndex].data[i] == null)
+                            options.series[seriesIndex].data.push(0);
+                    }
+                }
+                options.series[seriesIndex].data.push(parseInt(dept_data.count));
+
+                if (length == index+1)
+                    $('#chart-container').highcharts(options);
+            });
+        }
+    });
+});
+
+$('#report6-nav-item').click(function() {
+    var $data = getData(window.BASE_URL + '/rest/reports/yearly?category=individual');
+
+    $data.success(function (data) {
+        if (data.success)
+        {
+            var options = {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: $('#report6-nav-item').text()
+                },
+                xAxis: {
+                    categories: []
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Total Filed Sick Leave'
+                    }
+                },
+                plotOptions: {
+                    line: {
+                        dataLabels: {
+                            enabled: true
+                        },
+                        enableMouseTracking: false
+                    }
+                },
+                series: []
+            };
+
+            var length = data.data.collection.length;
+            $.each(data.data.collection, function (index, ind_data) {
+                var xIndex = $.inArray(ind_data.year, options.xAxis.categories);
+                if (xIndex == -1)
+                {
+                    options.xAxis.categories.push(ind_data.year);
                     xIndex = options.xAxis.categories.length-1;
                 }
 
