2ce7f077f6d2b32b5309dc73e97c8828c725b911
rdognidon <rdognidon@openit.com>
2015-09-08 11:25:53 +0800

    view sick leave for not admin

---
+++ ui/Backbones/see-history.php
@@ -0,0 +1,11 @@
+<?php
+
+//$path = ROOT_PATH . '/assets/tests/employee_details.csv';
+//$contents = file_get_contents($path);
+
+//die(str_replace(PHP_EOL, '<br>', CSV::EscapeCSV($contents)));
+
+//print_r(CSV::Parse($path));
+
+
+?>
